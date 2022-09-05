031289166ee921574dcc06415b5a3510420ae01e
EnsarDzaf0 <ensardzafo03@gmail.com>
2022-09-05 14:16:00 +0200

    current price + bids model
    added to listing model a current price row that will be used in bids and created bids model to store bids in auctions

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,9 +1,10 @@
 from http.client import USE_PROXY
 from django.contrib import admin
 
-from .models import User,Listing,watchlist
+from .models import User,Listing,watchlist,bids
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Listing)
 admin.site.register(watchlist)
+admin.site.register(bids)
--- auctions/models.py
+++ auctions/models.py
@@ -18,7 +18,13 @@ class Listing(models.Model):
         max_length=10
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_listings', default=None)
+    current_price = models.DecimalField(max_digits=6 ,decimal_places=2, default=0.00)
 
 class watchlist(models.Model):
     item = models.ForeignKey(Listing, on_delete=models.CASCADE,related_name="itemwatchlist")
     buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="buyerwatchlist")
+
+class bids(models.Model):
+    bid_item = models.ForeignKey(Listing, on_delete=models.CASCADE,related_name="itembid")
+    bid_buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="buyerbid")
+    bid_amount = models.DecimalField(max_digits=6 ,decimal_places=2, default=0.00)
